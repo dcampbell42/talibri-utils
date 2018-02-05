a70889345e55e80e62bba60fe648a396fa054a84
superstretch <acatzr800@gmail.com>
2018-02-05 14:48:57 -0500

    Updated colors, buttons, and layouts

--- talibri-chemo.user.js
+++ talibri-chemo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Talibri Chemo
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.1.0
 // @description  Remove some of the Cancer
 // @author       Dan Campbell
 // @match        https://www.talibri.com/*
@@ -10,16 +10,32 @@
 // ==/UserScript==
 
 // BEGIN EDITING
-var highlightColor  = '#0c0'; // hex color code or recognized color code
-var darkMode = true;   // true or false
+var highlightColor     = '#0c0'; // hex color code or recognized color code
+var textColor          = '#ccc'; // hex color code or recognized color code
+var chatColor          = '#ccc'; // hex color code or recognized color code
+var btnColor           = '#5bc0de';
+var primaryBtnColor    = '#069';
+var goodBtnColor       = '#0c0';
+var badBtnColor        = '#c00';
+var starColor          = '#ccc';
+
+var darkMode           = true;   // true or false
 var displayChatHeading = false; // true or false
 // END EDITING
 
-// and if you screwed something up, here are the default values:
+/* and if you screwed something up, here are the default values:
 
-//var hpColor  = 'green'; // hex color code or recognized color code
-//var darkMode = false;   // true or false
-//var displayChatHeading = true; // true or false
+var highlightColor     = '#0c0'; // hex color code or recognized color code
+var textColor          = '#ccc'; // hex color code or recognized color code
+var chatColor          = '#ccc'; // hex color code or recognized color code
+var btnColor           = '#5bc0de';
+var primaryBtnColor    = '#069';
+var goodBtnColor       = '#5cb85c';
+var badBtnColor        = '#d9534f';
+var starColor          = '#ccc';
+var darkMode           = true;   // true or false
+var displayChatHeading = false; // true or false
+*/
 
 (function() {
     'use strict';
@@ -27,6 +43,17 @@ var displayChatHeading = false; // true or false
     //global styles
     //reset body height to 100% of window
     addGlobalStyle('html, body { height: 100%; }');
+    //change colors
+    addGlobalStyle('body { color:'+textColor+' !important; }');
+    addGlobalStyle('.btn { background-color: '+btnColor+'; border-color: black; color:#fff; }');
+    addGlobalStyle('.btn:hover { background-color: '+btnColor+'; border-color: black; filter: brightness(120%); }');
+    addGlobalStyle('.btn-primary { background-color:'+primaryBtnColor+' !important;}');
+    addGlobalStyle('.btn-primary:hover { background-color:'+goodBtnColor+' !important;}');
+    addGlobalStyle('.btn-danger { background-color: '+badBtnColor+' !important;}');
+    addGlobalStyle('.btn-danger:hover { background-color: '+badBtnColor+' !important;}');
+    addGlobalStyle('.btn-success { background-color:'+goodBtnColor+' !important;}');
+    addGlobalStyle('.btn-success:hover { background-color:'+goodBtnColor+' !important;}');
+    addGlobalStyle('.fa-star,.fa-star-half-o { color:'+starColor+' !important;}');
 
     //set static height
     addGlobalStyle('.navbar-fixed-top { height:50px !important }');
@@ -39,6 +66,7 @@ var displayChatHeading = false; // true or false
     addGlobalStyle('body>.container-fluid:first-of-type>div.row>div.col-xs-3 { padding-left:0 !important; height:calc(100vh - 228px) !important; position: fixed !important; width:20%; }');
     addGlobalStyle('.main-chat-panel.panel-heading { height:37px !important; }');
     addGlobalStyle('#messages { height:calc(100vh - 228px) !important; }');
+    addGlobalStyle('#messages .card-text { color: '+chatColor+' !important;}');
     addGlobalStyle('.main-page { margin-left:20% !important; width:80% !important; margin-right:0 !important; padding-right:0 !important; }');
     addGlobalStyle('.main-chat-panel .form-group>br,.main-chat-panel .form-group>.text-muted {display:none; }');
     addGlobalStyle('.main-chat-panel .panel-footer { height:80px !important; }');
@@ -50,18 +78,25 @@ var displayChatHeading = false; // true or false
 
     //login styles
     addGlobalStyle('.jumbotron { background-color:rgba(0,0,0,0.75) !important; }');
-    addGlobalStyle('.jumbotron .btn-success { background-color:'+highlightColor+' !important; border-color:black }');
 
     //dashboard styles
     addGlobalStyle('.main-page .jumbotron { display:none !important; }');
 
     //inventory styles
-    addGlobalStyle('.inventory-panel { max-height:none !important; overflow-y:visible !important; }');
+    addGlobalStyle('.inventory-panel { max-height:none !important; overflow-y:visible !important; margin-bottom: 75px;}');
     addGlobalStyle('.inventory-panel>.panel-body { max-height:none !important; overflow-y:visible !important;  }');
 
     //profile styles
-    addGlobalStyle('#profile-main-div .col-xs-8 { max-height:none !important; overflow-y:visible !important; }');
+    addGlobalStyle('#profile-main-div .col-xs-8 { max-height:none !important; overflow-y:visible !important; margin-bottom:75px;}');
     addGlobalStyle('.progress-bar {background-color:'+highlightColor+'}');
+    addGlobalStyle('.main-page>.well-transparent {margin-bottom: 75px; !important; max-height:100vh !important;}');
+    addGlobalStyle('#profile-main-div .col-xs-8 .panel-success .panel-body { max-height:none !important; overflow-y:visible !important;}');
+
+    //leaderboard styles
+    addGlobalStyle('.leaderboard-panel {overflow-y:visible !important; max-height:none !important; margin-bottom: 75px; }');
+
+    //market styles
+    addGlobalStyle('div.col-xs-9.main-page .panel-success {margin-bottom: 75px;}');
 
     if(darkMode) enterDarkMode();
     if(displayChatHeading == false) {
@@ -89,7 +124,6 @@ function enterDarkMode(){
     addGlobalStyle('.percentage-circle-contents>img { filter: invert(100%); }');
     addGlobalStyle('.percentage-circle-fg {fill: black;}');
     addGlobalStyle('.panel, .breadcrumb { background-color: rgba(0,0,0,0.5) !important; }');
-    addGlobalStyle('body { color:#ccc !important; }');
     addGlobalStyle('a { color:'+highlightColor+' !important; }');
     addGlobalStyle('.main-chat-panel .panel-footer { background-color:rgba(0,0,0,0.5) !important; }');
     addGlobalStyle('.panel-success>.panel-heading, .alert-success, .panel-default>.panel-heading, .panel-info>.panel-heading, .panel-primary>.panel-heading {color: #ccc; background-color: #333; border-color: #666;}');
@@ -97,8 +131,6 @@ function enterDarkMode(){
     addGlobalStyle('.dropdown-menu,.list-group-item,.well { background-color:#333; }');
     addGlobalStyle('.dropdown-menu a:hover { background-color:#111 !important; }');
     addGlobalStyle('.nav-tabs>li.active>a, .nav-tabs>li.active>a:focus, .nav-tabs>li>a:hover{ background-color:#333 !important; }');
-    addGlobalStyle('.actions button.btn-sm {background-color: '+highlightColor+'; border-color:black; color: white;}');
-    addGlobalStyle('.actions button.btn-sm.btn-danger {background-color: #d9534f; border-color:black; color: white; }');
     addGlobalStyle('.table>tbody>tr.success>td, .table>tbody>tr.success>th, .table>tbody>tr>td.success, .table>tbody>tr>th.success, .table>tfoot>tr.success>td, .table>tfoot>tr.success>th, .table>tfoot>tr>td.success, .table>tfoot>tr>th.success, .table>thead>tr.success>td, .table>thead>tr.success>th, .table>thead>tr>td.success, .table>thead>tr>th.success{background-color: rgba(255,255,255,0.25) !important;}');
     addGlobalStyle('.panel-footer {background-color:rgba(0,0,0,0.2)}');
     addGlobalStyle('select {background-color:#333}');
@@ -107,4 +139,10 @@ function enterDarkMode(){
     addGlobalStyle('.popover { background-color: #333 !important}');
     addGlobalStyle('.popover-title { background-color: #333 !important}');
     addGlobalStyle('#main-chat-text-area {background-color: #333 !important;color: #ccc !important}');
+    addGlobalStyle('#progressBarContainer {background-color:#f5f5f5 !important;border-radius: 4px;-webkit-box-shadow: inset 0 1px 2px rgba(0,0,0,0.1);box-shadow: inset 0 1px 2px rgba(0,0,0,0.1);}');
+    addGlobalStyle('#progressBar {background-color:'+highlightColor+' !important;}');
+    addGlobalStyle('#user-stat-actions { color: black !important; padding-top:2px !important;}');
+    addGlobalStyle('.nav>li>a:hover { background-color: #333;');
+    //modal
+    addGlobalStyle('.modal-content{ background-color:#333; }');
 }
